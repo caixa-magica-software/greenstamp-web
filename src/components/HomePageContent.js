667035d856b40538c34bbf054aeba1dbc88556eb
rodrigo.resende <rodrigo.resende@caixamagica.pt>
2023-04-13 15:52:21 +0100

    Added a function that requests analysis data

--- src/components/HomePageContent.js
+++ src/components/HomePageContent.js
@@ -1,6 +1,21 @@
 import { Fragment } from "react";
+import { analyzeURL } from "../config/api";
 
 const HomePageContent = () => {
+  const requestData = () => {
+    fetch(analyzeURL, {
+      method: "POST",
+      body: JSON.stringify({
+        appName: "RTP Notícias",
+        packageName: "pt.rtp.noticias.android",
+        version: 136,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  };
+
   return (
     <Fragment>
       <p>Hello World</p>
---
+++ src/config/api.js
@@ -0,0 +1,1 @@
+export const analyzeURL = "http://51.210.255.156:3000/api/analyze";
