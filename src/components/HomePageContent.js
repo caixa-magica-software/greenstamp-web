4b3d045bf32c20fed2cdac34baae4c7579c5b869
guilherme.enes <guilherme.enes@caixamagica.pt>
2023-04-13 15:50:43 +0100

    Added basic styling and page layout to the home page

--- src/components/HomePageContent.js
+++ src/components/HomePageContent.js
@@ -1,9 +1,18 @@
 import { Fragment } from "react";
+import classes from "./HomePageContent.module.css";
 
 const HomePageContent = () => {
   return (
     <Fragment>
-      <p>Hello World</p>
+      <div className={classes.leftBox}>
+        <img className={classes.logoImg} src="/logo192.png" alt="Logo" />
+        <p className={classes.logoHeader}>Mobile Energy Efficiency Services</p>
+        <p className={classes.pageHeader}>Green Stamp</p>
+      </div>
+
+      <div className={classes.rightBox}>
+        <p className={classes.results}>Test</p>
+      </div>
     </Fragment>
   );
 };
