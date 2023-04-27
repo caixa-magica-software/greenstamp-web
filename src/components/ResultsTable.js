00cce00836b1f2635417b9463c25d643a857e1a3
rodrigo.resende <rodrigo.resende@caixamagica.pt>
2023-04-27 18:13:03 +0100

    Made the fetched data properly display on the table

--- src/components/ResultsTable.js
+++ src/components/ResultsTable.js
@@ -1,4 +1,4 @@
-import { useMemo, useState, useEffect, useRef } from "react";
+import { useMemo, useEffect, useRef, useState } from "react";
 import classes from "./ResultsTable.module.css";
 import axios from "axios";
 import {
@@ -11,85 +11,130 @@ import {
 } from "react-table";
 
 const ResultsTable = () => {
-  const [tabledata, setTableData] = useState([]);
   const hasFetched = useRef(false);
+  const hasMapped = useRef(false);
+  const [dataArray, setDataArray] = useState([{}]);
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: "App",
+        accessor: "app_name",
+      },
+      {
+        Header: "Package",
+        accessor: "package",
+      },
+      {
+        Header: "Version",
+        accessor: "version",
+      },
+      {
+        Header: "Date",
+        accessor: "timestamp",
+      },
+      {
+        Header: "Test",
+        accessor: "test_name",
+      },
+      {
+        Header: "Parameter",
+        accessor: "test_parameter",
+      },
+      {
+        Header: "Result",
+        accessor: "test_result",
+      },
+    ],
+    []
+  );
+
+  const data = useMemo(() => dataArray, [dataArray]);
 
   useEffect(() => {
     const fetchAllData = async () => {
       try {
         const res = await axios.get("http://localhost:8800");
-        setTableData(res.data);
+        const responseData = res.data;
         hasFetched.current = true;
+        if (hasMapped.current === false && dataArray.length === 1) {
+          setDataArray(responseData);
+
+          console.log(dataArray);
+          if (dataArray.length > 1) hasMapped.current = true;
+        }
       } catch (err) {
         console.log(err);
       }
     };
-    if (hasFetched.current === false) fetchAllData();
-  }, []);
+
+    if (hasFetched.current === false || hasMapped === false) {
+      fetchAllData();
+    }
+  }, [dataArray]);
+
+  const table = useTable(
+    { columns, data },
+    useFilters,
+    useGroupBy,
+    useSortBy,
+    useExpanded,
+    usePagination
+  );
+
+  const { getTableProps, headerGroups, rows, prepareRow } = table;
+
+  return (
+    <div className={classes.tablePosition}>
+      <table {...getTableProps()}>
+        <thead>
+          {headerGroups.map((headerGroup) => (
+            // Apply the header row props
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {
+                // Loop over the headers in each row
+                headerGroup.headers.map((column) => (
+                  // Adds sorting controls into the table headers.
+                  <th
+                    className={classes.noSelect}
+                    {...column.getHeaderProps(column.getSortByToggleProps())}
+                  >
+                    {column.render("Header")}
+                    {/* Add a sort direction indicator */}
+                    <span>
+                      {column.isSorted ? (column.isSortedDesc ? "↓" : "↑") : ""}
+                    </span>
+                  </th>
+                ))
+              }
+            </tr>
+          ))}
+        </thead>
+        <tbody>
+          {rows.map((row) => {
+            // Prepare the row for display
+            prepareRow(row);
+            return (
+              // Apply the row props
+              <tr {...row.getRowProps()}>
+                {row.cells.map((cell) => {
+                  // Loop over the rows cells
+                  return (
+                    // Apply the cell props
+                    <td {...cell.getCellProps()}>
+                      {
+                        cell.render("Cell") // Render the cell contents
+                      }
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
-
-//   const table = useTable(
-//     { columns, data },
-//     useFilters,
-//     useGroupBy,
-//     useSortBy,
-//     useExpanded,
-//     usePagination
-//   );
-
-//   const { getTableProps, headerGroups, rows, prepareRow } = table;
-
-//   return (
-//     <div className={classes.tablePosition}>
-//       <table {...getTableProps()}>
-//         <thead>
-//           {headerGroups.map((headerGroup) => (
-//             // Apply the header row props
-//             <tr {...headerGroup.getHeaderGroupProps()}>
-//               {
-//                 // Loop over the headers in each row
-//                 headerGroup.headers.map((column) => (
-//                   // Adds sorting controls into the table headers.
-//                   <th
-//                     className={classes.noSelect}
-//                     {...column.getHeaderProps(column.getSortByToggleProps())}
-//                   >
-//                     {column.render("Header")}
-//                     {/* Add a sort direction indicator */}
-//                     <span>
-//                       {column.isSorted ? (column.isSortedDesc ? "↓" : "↑") : ""}
-//                     </span>
-//                   </th>
-//                 ))
-//               }
-//             </tr>
-//           ))}
-//         </thead>
-//         <tbody>
-//           {rows.map((row) => {
-//             // Prepare the row for display
-//             prepareRow(row);
-//             return (
-//               // Apply the row props
-//               <tr {...row.getRowProps()}>
-//                 {row.cells.map((cell) => {
-//                   // Loop over the rows cells
-//                   return (
-//                     // Apply the cell props
-//                     <td {...cell.getCellProps()}>
-//                       {
-//                         cell.render("Cell") // Render the cell contents
-//                       }
-//                     </td>
-//                   );
-//                 })}
-//               </tr>
-//             );
-//           })}
-//         </tbody>
-//       </table>
-//     </div>
-//   );
-// };
 
 export default ResultsTable;
