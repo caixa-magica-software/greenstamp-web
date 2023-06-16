5f16560d93ac3e7c1e7a0228dc01aa51b976e8e4
Adriano Campos <adrianoribeirocampos@gmail.com>
2023-06-16 17:00:38 +0100

    Add developer service env configuration

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,4 @@
-export const dbGetAll = "http://localhost:3000/api/get-all";
-export const dbGetAllFormatted = "http://localhost:3000/api/get-all-formatted";
-export const getAppInfo = "https://ws75.aptoide.com/api/7/getApp?package_name=";
-export const analyzeApp = "http://localhost:3000/api/analyze";
+export const dbGetAll = process.env.DEVELOPER_SERVICE + "/api/get-all";
+export const dbGetAllFormatted = process.env.DEVELOPER_SERVICE + "/api/get-all-formatted";
+export const getAppInfo = process.env.DEVELOPER_SERVICE + "/api/7/getApp?package_name=";
+export const analyzeApp = process.env.DEVELOPER_SERVICE + "/api/analyze";
